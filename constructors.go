bf1cecc31034bb971dde766f75d9829f67c5230a
Dave Fort <dhfort@gmail.com>
2019-05-03 11:06:08 -0700

    top-level constructor does not return error

--- constructors.go
+++ constructors.go
@@ -1,9 +1,17 @@
 package pd
 
-import "github.com/ptiger10/pd/series"
+import (
+	"log"
+
+	"github.com/ptiger10/pd/series"
+)
 
 // Series is the default Series constructor.
 // For more configuration options (e.g., custom index), use pd/series.New()
-func Series(data interface{}) (series.Series, error) {
-	return series.New(data)
+func Series(data interface{}) series.Series {
+	s, err := series.New(data)
+	if err != nil {
+		log.Printf("Unable to create series: %v", err)
+	}
+	return s
 }
