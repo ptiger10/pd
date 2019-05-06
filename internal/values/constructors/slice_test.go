d78c35f93009a24a64d9a3b9a0172890175bc271
Dave Fort <dhfort@gmail.com>
2019-05-06 09:58:26 -0700

    Value constructor edge cases

--- internal/values/constructors/slice_test.go
+++ internal/values/constructors/slice_test.go
@@ -1,82 +1,138 @@
 package constructors
 
-// func TestSliceConstructor(t *testing.T) {
-// 	testDate := time.Date(2019, 5, 1, 0, 0, 0, 0, time.UTC)
-// 	var tests = []struct {
-// 		data     interface{}
-// 		wantVals values.Values
-// 		wantKind kinds.Kind
-// 	}{
-// 		{
-// 			data:     []float64{0, 1, 2},
-// 			wantVals: SliceFloat([]float64{0, 1, 2}),
-// 			wantKind: kinds.Float,
-// 		},
-// 		{
-// 			data:     []int{0, 1, 2},
-// 			wantVals: SliceInt([]int64{0, 1, 2}),
-// 			wantKind: kinds.Int,
-// 		},
-// 		{
-// 			data:     []uint{0, 1, 2},
-// 			wantVals: SliceInt([]int64{0, 1, 2}),
-// 			wantKind: kinds.Int,
-// 		},
-// 		{
-// 			data:     []string{"0", "1", "2"},
-// 			wantVals: SliceString([]string{"0", "1", "2"}),
-// 			wantKind: kinds.String,
-// 		},
-// 		{
-// 			data:     []bool{true, true, false},
-// 			wantVals: SliceBool([]bool{true, true, false}),
-// 			wantKind: kinds.Bool,
-// 		},
-// 		{
-// 			data:     []time.Time{testDate},
-// 			wantVals: SliceDateTime([]time.Time{testDate}),
-// 			wantKind: kinds.DateTime,
-// 		},
-// 		// {
-// 		// 	data:     []time.Time{time.Time{}},
-// 		// 	wantVals: SliceDateTime([]time.Time{}),
-// 		// 	wantKind: kinds.DateTime,
-// 		// },
-// 		{
-// 			data:     []interface{}{1.5, 1, "", false, testDate},
-// 			wantVals: SliceInterface([]interface{}{1.5, 1, "", false, testDate}),
-// 			wantKind: kinds.Interface,
-// 		},
-// 	}
-// 	for _, test := range tests {
-// 		vals, err := ValuesFromSlice(test.data)
-// 		if err != nil {
-// 			t.Errorf("Unable to construct values from %v: %v", test.data, err)
-// 		}
-// 		if !reflect.DeepEqual(vals.V, test.wantVals) {
-// 			t.Errorf("%T test returned values %#v, want %#v", test.data, vals, test.wantVals)
-// 		}
-// 		if vals.Kind != test.wantKind {
-// 			t.Errorf("%T test returned value %v, want %v", test.data, vals.Kind, test.wantKind)
-// 		}
-// 	}
-// }
+import (
+	"math"
+	"reflect"
+	"testing"
+	"time"
 
-// func TestSliceConstructor_NullFloat(t *testing.T) {
-// 	vals, err := ValuesFromSlice([]float64{math.NaN()})
-// 	if err != nil {
-// 		t.Errorf("Unable to construct values from null float: %v", err)
-// 	}
-// 	val := vals.V.Element(0)[0].(float64)
-// 	if !math.IsNaN(val) {
-// 		t.Errorf("Returned %v, want NaN", val)
-// 	}
-// }
+	"github.com/ptiger10/pd/internal/values"
+	"github.com/ptiger10/pd/kinds"
+)
 
-// func TestSliceConstructor_Unsupported(t *testing.T) {
-// 	data := []complex64{1, 2, 3}
-// 	_, err := ValuesFromSlice(data)
-// 	if err == nil {
-// 		t.Errorf("Returned nil error, expected error due to unsupported type %T", data)
-// 	}
-// }
+func TestSliceConstructor(t *testing.T) {
+	testDate := time.Date(2019, 5, 1, 0, 0, 0, 0, time.UTC)
+	var tests = []struct {
+		data     interface{}
+		wantVals values.Values
+		wantKind kinds.Kind
+	}{
+		{
+			data:     []float32{0, 1, 2},
+			wantVals: SliceFloat([]float64{0, 1, 2}).V,
+			wantKind: kinds.Float,
+		},
+		{
+			data:     []float64{0, 1, 2},
+			wantVals: SliceFloat([]float64{0, 1, 2}).V,
+			wantKind: kinds.Float,
+		},
+		{
+			data:     []float64{},
+			wantVals: SliceFloat([]float64{}).V,
+			wantKind: kinds.Float,
+		},
+		{
+			data:     []int{0, 1, 2},
+			wantVals: SliceInt([]int64{0, 1, 2}).V,
+			wantKind: kinds.Int,
+		},
+		{
+			data:     []int64{0, 1, 2},
+			wantVals: SliceInt([]int64{0, 1, 2}).V,
+			wantKind: kinds.Int,
+		},
+		{
+			data:     []int{},
+			wantVals: SliceInt([]int64{}).V,
+			wantKind: kinds.Int,
+		},
+		{
+			data:     []uint{0, 1, 2},
+			wantVals: SliceInt([]int64{0, 1, 2}).V,
+			wantKind: kinds.Int,
+		},
+		{
+			data:     []string{"0", "1", ""},
+			wantVals: SliceString([]string{"0", "1", "NaN"}).V,
+			wantKind: kinds.String,
+		},
+		{
+			data:     []string{},
+			wantVals: SliceString([]string{}).V,
+			wantKind: kinds.String,
+		},
+		{
+			data:     []bool{true, true, false},
+			wantVals: SliceBool([]bool{true, true, false}).V,
+			wantKind: kinds.Bool,
+		},
+		{
+			data:     []bool{},
+			wantVals: SliceBool([]bool{}).V,
+			wantKind: kinds.Bool,
+		},
+		{
+			data:     []time.Time{testDate, time.Time{}},
+			wantVals: SliceDateTime([]time.Time{testDate, time.Time{}}).V,
+			wantKind: kinds.DateTime,
+		},
+		{
+			data:     []time.Time{},
+			wantVals: SliceDateTime([]time.Time{}).V,
+			wantKind: kinds.DateTime,
+		},
+		{
+			data:     []interface{}{1.5, 1, "", false, testDate},
+			wantVals: SliceInterface([]interface{}{1.5, 1, "", false, testDate}).V,
+			wantKind: kinds.Interface,
+		},
+		{
+			data:     []interface{}{},
+			wantVals: SliceInterface([]interface{}{}).V,
+			wantKind: kinds.Interface,
+		},
+	}
+	for _, test := range tests {
+		vals, err := ValuesFromSlice(test.data)
+		if err != nil {
+			t.Errorf("Unable to construct values from %v: %v", test.data, err)
+		}
+		if !reflect.DeepEqual(vals.V, test.wantVals) {
+			t.Errorf("%T test returned values %#v, want %#v", test.data, vals, test.wantVals)
+		}
+		if vals.Kind != test.wantKind {
+			t.Errorf("%T test returned value %v, want %v", test.data, vals.Kind, test.wantKind)
+		}
+	}
+}
+
+func TestSliceConstructor_NullFloat(t *testing.T) {
+	vals, err := ValuesFromSlice([]float64{math.NaN()})
+	if err != nil {
+		t.Errorf("Unable to construct values from null float: %v", err)
+	}
+	val := vals.V.Element(0)[0].(float64)
+	if !math.IsNaN(val) {
+		t.Errorf("Returned %v, want NaN", val)
+	}
+}
+
+func TestSliceConstructor_NullFloatInterface(t *testing.T) {
+	vals, err := ValuesFromSlice([]interface{}{math.NaN()})
+	if err != nil {
+		t.Errorf("Unable to construct values from null float: %v", err)
+	}
+	val := vals.V.Element(0)[0].(float64)
+	if !math.IsNaN(val) {
+		t.Errorf("Returned %v, want NaN", val)
+	}
+}
+
+func TestSliceConstructor_Unsupported(t *testing.T) {
+	data := []complex64{1, 2, 3}
+	_, err := ValuesFromSlice(data)
+	if err == nil {
+		t.Errorf("Returned nil error, expected error due to unsupported type %T", data)
+	}
+}
--- internal/values/constructors/t-datetimeFactory.go
+++ internal/values/constructors/t-datetimeFactory.go
@@ -12,9 +12,6 @@ import (
 // SliceDateTime converts []time.Time{} -> ValuesFactory with values.DateTimeValues
 func SliceDateTime(vals []time.Time) ValuesFactory {
 	var v values.DateTimeValues
-	if len(vals) == 0 {
-		return ValuesFactory{nil, kinds.DateTime}
-	}
 	for _, val := range vals {
 		if (time.Time{}) == val {
 			v = append(v, values.DateTime(val, true))
