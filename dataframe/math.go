e37f28ec7c2bb5accab863edba9632a7ce812545
Dave Fort <dhfort@gmail.com>
2019-06-17 21:42:21 -0700

    dataframe math operations

--- dataframe/math.go
+++ dataframe/math.go
@@ -10,6 +10,10 @@ import (
 )
 
 func (df *DataFrame) math(name string, fn func(s *series.Series) float64) *series.Series {
+	if df == nil {
+		s, _ := series.New(nil)
+		return s
+	}
 	var vals []interface{}
 	var idx []interface{}
 	for _, s := range df.s {
@@ -34,4 +38,19 @@ func (df *DataFrame) Sum() *series.Series {
 // Mean all numerical or boolean columns.
 func (df *DataFrame) Mean() *series.Series {
 	return df.math("mean", (*series.Series).Mean)
+}
+
+// Min all numerical columns.
+func (df *DataFrame) Min() *series.Series {
+	return df.math("min", (*series.Series).Min)
+}
+
+// Max all numerical columns.
+func (df *DataFrame) Max() *series.Series {
+	return df.math("max", (*series.Series).Max)
+}
+
+// Std returns the standard deviation of all numerical columns.
+func (df *DataFrame) Std() *series.Series {
+	return df.math("std", (*series.Series).Std)
 }
--- dataframe/math_test.go
+++ dataframe/math_test.go
@@ -1,42 +1,43 @@
 package dataframe
 
 import (
+	"math"
 	"testing"
 
 	"github.com/ptiger10/pd/series"
 )
 
 func Test_Math(t *testing.T) {
-	type args struct {
-		data   []interface{}
-		config Config
-	}
 	type want struct {
 		data   interface{}
 		config series.Config
 	}
 	tests := []struct {
 		name string
-		args args
 		fn   func(*DataFrame) *series.Series
 		want want
 	}{
-		{
-			"Sum",
-			args{[]interface{}{[]float64{1, 2}, []float64{3, 4}}, Config{Cols: []interface{}{"foo", "bar"}}},
-			(*DataFrame).Sum,
-			want{[]float64{3, 7}, series.Config{Index: []string{"foo", "bar"}, Name: "sum"}},
+		{"Sum", (*DataFrame).Sum,
+			want{[]float64{9, -4, 0}, series.Config{Index: []string{"foo", "bar", "baz"}, Name: "sum"}},
 		},
-		{
-			"Mean",
-			args{[]interface{}{[]float64{1, 3, 5}, []float64{-3, -2, -1}, []float64{-5, 0, 5}}, Config{Cols: []interface{}{"foo", "bar", "baz"}}},
-			(*DataFrame).Mean,
+		{"Mean", (*DataFrame).Mean,
 			want{[]float64{3, -2, 0}, series.Config{Index: []string{"foo", "bar", "baz"}, Name: "mean"}},
+		},
+		{"Min", (*DataFrame).Min,
+			want{[]float64{1, -3, -5}, series.Config{Index: []string{"foo", "bar", "baz"}, Name: "min"}},
+		},
+		{"Max", (*DataFrame).Max,
+			want{[]float64{5, -1, 5}, series.Config{Index: []string{"foo", "bar", "baz"}, Name: "max"}},
+		},
+		{"Std", (*DataFrame).Std,
+			want{[]float64{1.632993161855452, 1, 4.08248290463863}, series.Config{Index: []string{"foo", "bar", "baz"}, Name: "std"}},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			df, err := New(tt.args.data, tt.args.config)
+			df, err := New(
+				[]interface{}{[]float64{1, 3, 5}, []float64{-3, math.NaN(), -1}, []float64{-5, 0, 5}},
+				Config{Cols: []interface{}{"foo", "bar", "baz"}})
 			if err != nil {
 				t.Errorf("%v(): %v", tt.name, err)
 			}
