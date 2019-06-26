b97332895761c684fc0d8487f1c00ca61b069179
Dave Fort <dhfort@gmail.com>
2019-06-26 14:19:24 -0700

    consolidated values factories

---
+++ internal/values/factory.go
@@ -0,0 +1,217 @@
+package values
+
+import (
+	"fmt"
+	"reflect"
+	"time"
+
+	"github.com/ptiger10/pd/options"
+)
+
+// InterfaceFactory converts interface{} to Values
+func InterfaceFactory(data interface{}) (Factory, error) {
+	var factory Factory
+	var err error
+	if data == nil {
+		factory = Factory{Values: emptyValues(), DataType: options.None}
+	} else {
+		switch reflect.ValueOf(data).Kind() {
+		case reflect.Float32, reflect.Float64,
+			reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64,
+			reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64,
+			reflect.String,
+			reflect.Bool,
+			reflect.Struct:
+			factory, err = ScalarFactory(data)
+
+		case reflect.Slice:
+			factory, err = SliceFactory(data)
+
+		default:
+			return Factory{Values: emptyValues()}, fmt.Errorf("internal.values.InterfaceFactory(): type not supported: %T", data)
+		}
+	}
+	return factory, err
+}
+
+// ScalarFactory creates a Factory from an interface{} that has been determined elsewhere to be a scalar.
+// Be careful modifying this constructor and its children,
+// as reflection is inherently unsafe and each function expects to receive specific types only.
+func ScalarFactory(data interface{}) (Factory, error) {
+	var ret Factory
+	switch data.(type) {
+	case float32, float64:
+		val := scalarFloatToFloat64(data)
+		f := newFloat64(val)
+		ret = Factory{&float64Values{f}, options.Float64}
+
+	case int, int8, int16, int32, int64:
+		val := scalarIntToInt64(data)
+		i := newInt64(val)
+		ret = Factory{&int64Values{i}, options.Int64}
+
+	case uint, uint8, uint16, uint32, uint64:
+		val := scalarUIntToInt64(data)
+		i := newInt64(val)
+		ret = Factory{&int64Values{i}, options.Int64}
+
+	case string:
+		val := newString(data.(string))
+		ret = Factory{&stringValues{val}, options.String}
+
+	case bool:
+		val := newBool(data.(bool))
+		ret = Factory{&boolValues{val}, options.Bool}
+
+	case time.Time:
+		val := newDateTime(data.(time.Time))
+		ret = Factory{&dateTimeValues{val}, options.DateTime}
+
+	// case interface{}:
+	// 	val := newInterface(data.(interface{}))
+	// 	ret = Factory{&interfaceValues{val}, options.Interface}
+
+	default:
+		ret = Factory{}
+		return ret, fmt.Errorf("Type %T not supported", data)
+	}
+
+	return ret, nil
+}
+
+// scalarFloatToFloat64 converts a known float interface{} -> float64
+func scalarFloatToFloat64(data interface{}) float64 {
+	d := reflect.ValueOf(data)
+	return d.Float()
+}
+
+// scalarIntToInt64 converts a known int interface{} -> int64
+func scalarIntToInt64(data interface{}) int64 {
+	d := reflect.ValueOf(data)
+	return d.Int()
+}
+
+// scalarUIntToInt64 converts a known uint interface{} -> int64
+func scalarUIntToInt64(data interface{}) int64 {
+	d := reflect.ValueOf(data)
+	return int64(d.Uint())
+}
+
+// SliceFactory creates a Factory from an interface{} that has been determined elsewhere to be a Slice.
+// Be careful modifying this constructor and its children,
+// as reflection is inherently unsafe and each function expects to receive specific types only.
+func SliceFactory(data interface{}) (Factory, error) {
+	var ret Factory
+
+	switch data.(type) {
+	case []float32:
+		vals := sliceFloatToSliceFloat64(data)
+		ret = newSliceFloat64(vals)
+
+	case []float64:
+		vals := data.([]float64)
+		ret = newSliceFloat64(vals)
+
+	case []int, []int8, []int16, []int32:
+		vals := sliceIntToSliceInt64(data)
+		ret = newSliceInt64(vals)
+	case []int64:
+		vals := data.([]int64)
+		ret = newSliceInt64(vals)
+
+	case []uint, []uint8, []uint16, []uint32, []uint64:
+		// converts into []int64 so it can be passed into SliceInt
+		vals := sliceUIntToSliceInt64(data)
+		ret = newSliceInt64(vals)
+
+	case []string:
+		vals := data.([]string)
+		ret = newSliceString(vals)
+
+	case []bool:
+		vals := data.([]bool)
+		ret = newSliceBool(vals)
+
+	case []time.Time:
+		vals := data.([]time.Time)
+		ret = newSliceDateTime(vals)
+
+	case []interface{}:
+		vals := data.([]interface{})
+		ret = newSliceInterface(vals)
+
+	default:
+		ret = Factory{nil, options.None}
+		return ret, fmt.Errorf("Type %T not supported", data)
+	}
+
+	return ret, nil
+}
+
+// [START interface converters]
+
+// sliceFloatToSliceFloat64 converts known []float interface{} -> []float64
+func sliceFloatToSliceFloat64(data interface{}) []float64 {
+	var vals []float64
+	d := reflect.ValueOf(data)
+	for i := 0; i < d.Len(); i++ {
+		v := d.Index(i).Float()
+		vals = append(vals, v)
+	}
+	return vals
+}
+
+// sliceIntToSliceInt64 converts known []int interface{} -> []int64
+func sliceIntToSliceInt64(data interface{}) []int64 {
+	var vals []int64
+	d := reflect.ValueOf(data)
+	for i := 0; i < d.Len(); i++ {
+		v := d.Index(i).Int()
+		vals = append(vals, v)
+	}
+	return vals
+}
+
+// sliceUIntToSliceInt64 converts knonw []uint interface{} -> []int64
+func sliceUIntToSliceInt64(data interface{}) []int64 {
+	var vals []int64
+	d := reflect.ValueOf(data)
+	for i := 0; i < d.Len(); i++ {
+		v := d.Index(i).Uint()
+		vals = append(vals, int64(v))
+	}
+	return vals
+}
+
+// [END interface converters]
+
+// [START utility slices]
+
+// makeRange returns a sequential series of numbers, for use in the default Series index constructor.
+func makeRange(min, max int) []int64 {
+	a := make([]int64, max-min)
+	for i := range a {
+		a[i] = int64(min + i)
+	}
+	return a
+}
+
+// MakeIntRange returns a sequential series of numbers, for use in making default index labels.
+func MakeIntRange(min, max int) []int {
+	a := make([]int, max-min)
+	for i := range a {
+		a[i] = min + i
+	}
+	return a
+}
+
+// MakeInterfaceRange returns a sequential series of numbers as an interface slice, for use in making default column labels.
+func MakeInterfaceRange(min, max int) []interface{} {
+	a := make([]interface{}, max-min)
+	for i := range a {
+		a[i] = min + i
+	}
+	return a
+}
+
+// [END utility slices]
---
+++ internal/values/factory_test.go
@@ -0,0 +1,245 @@
+package values
+
+import (
+	"math"
+	"reflect"
+	"testing"
+	"time"
+
+	"github.com/ptiger10/pd/options"
+)
+
+func TestInterfaceFactory(t *testing.T) {
+	type args struct {
+		data interface{}
+	}
+	type want struct {
+		factory Factory
+		err     bool
+	}
+	tests := []struct {
+		name string
+		args args
+		want want
+	}{
+		{"nil", args{nil},
+			want{Factory{Values: &interfaceValues{}, DataType: options.None}, false}},
+		{"scalar", args{"foo"},
+			want{Factory{Values: &stringValues{stringValue{"foo", false}}, DataType: options.String}, false}},
+		{"slice", args{[]string{"foo"}},
+			want{Factory{Values: &stringValues{stringValue{"foo", false}}, DataType: options.String}, false}},
+		{"fail: unsupported", args{[1]string{"foo"}},
+			want{Factory{Values: &interfaceValues{}, DataType: options.None}, true}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := InterfaceFactory(tt.args.data)
+			if (err != nil) != tt.want.err {
+				t.Errorf("InterfaceFactory() error = %v, wantErr %v", err, tt.want.err)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want.factory) {
+				t.Errorf("InterfaceFactory() = %v, want %v", got, tt.want.factory)
+			}
+		})
+	}
+}
+
+func TestScalarFactory(t *testing.T) {
+	type args struct {
+		data interface{}
+	}
+	type want struct {
+		factory Factory
+		err     bool
+	}
+	tests := []struct {
+		name string
+		args args
+		want want
+	}{
+		{"float32",
+			args{float32(1.0)},
+			want{Factory{Values: &float64Values{float64Value{1.0, false}}, DataType: options.Float64}, false}},
+		{"float64",
+			args{float64(1.0)},
+			want{Factory{Values: &float64Values{float64Value{1.0, false}}, DataType: options.Float64}, false}},
+		{"int",
+			args{int(1)},
+			want{Factory{Values: &int64Values{int64Value{1, false}}, DataType: options.Int64}, false}},
+		{"int8",
+			args{int8(1)},
+			want{Factory{Values: &int64Values{int64Value{1, false}}, DataType: options.Int64}, false}},
+		{"int16",
+			args{int16(1)},
+			want{Factory{Values: &int64Values{int64Value{1, false}}, DataType: options.Int64}, false}},
+		{"int32",
+			args{int32(1)},
+			want{Factory{Values: &int64Values{int64Value{1, false}}, DataType: options.Int64}, false}},
+		{"int64",
+			args{int64(1)},
+			want{Factory{Values: &int64Values{int64Value{1, false}}, DataType: options.Int64}, false}},
+		{"uint",
+			args{uint(1)},
+			want{Factory{Values: &int64Values{int64Value{1, false}}, DataType: options.Int64}, false}},
+		{"uint8",
+			args{uint8(1)},
+			want{Factory{Values: &int64Values{int64Value{1, false}}, DataType: options.Int64}, false}},
+		{"uint16",
+			args{uint16(1)},
+			want{Factory{Values: &int64Values{int64Value{1, false}}, DataType: options.Int64}, false}},
+		{"uint32",
+			args{uint32(1)},
+			want{Factory{Values: &int64Values{int64Value{1, false}}, DataType: options.Int64}, false}},
+		{"uint64",
+			args{uint64(1)},
+			want{Factory{Values: &int64Values{int64Value{1, false}}, DataType: options.Int64}, false}},
+		{"string",
+			args{"foo"},
+			want{Factory{Values: &stringValues{stringValue{"foo", false}}, DataType: options.String}, false}},
+		{"string null",
+			args{""},
+			want{Factory{Values: &stringValues{stringValue{"NaN", true}}, DataType: options.String}, false}},
+		{"bool",
+			args{true},
+			want{Factory{Values: &boolValues{boolValue{true, false}}, DataType: options.Bool}, false}},
+		{"datetime",
+			args{time.Date(2019, 1, 1, 0, 0, 0, 0, time.UTC)},
+			want{Factory{Values: &dateTimeValues{dateTimeValue{time.Date(2019, 1, 1, 0, 0, 0, 0, time.UTC), false}}, DataType: options.DateTime}, false}},
+		{"unsupported",
+			args{complex64(1)},
+			want{Factory{Values: nil, DataType: options.None}, true}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := ScalarFactory(tt.args.data)
+			if (err != nil) != tt.want.err {
+				t.Errorf("ScalarFactory() error = %v, wantErr %v", err, tt.want.err)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want.factory) {
+				t.Errorf("ScalarFactory() = %v, want %v", got, tt.want.factory)
+			}
+		})
+	}
+}
+
+func TestScalarConstructor_NullFloat(t *testing.T) {
+	vals, err := ScalarFactory(math.NaN())
+	if err != nil {
+		t.Errorf("Unable to construct values from null float: %v", err)
+	}
+	val := vals.Values.Element(0).Value.(float64)
+	if !math.IsNaN(val) {
+		t.Errorf("Returned %v, want NaN", val)
+	}
+}
+
+func TestSliceFactory(t *testing.T) {
+	type args struct {
+		data interface{}
+	}
+	type want struct {
+		factory Factory
+		err     bool
+	}
+	tests := []struct {
+		name string
+		args args
+		want want
+	}{
+		{"float32",
+			args{[]float32{1.0}},
+			want{Factory{Values: &float64Values{float64Value{1.0, false}}, DataType: options.Float64}, false}},
+		{"float64",
+			args{[]float64{1.0}},
+			want{Factory{Values: &float64Values{float64Value{1.0, false}}, DataType: options.Float64}, false}},
+		{"int",
+			args{[]int{1}},
+			want{Factory{Values: &int64Values{int64Value{1, false}}, DataType: options.Int64}, false}},
+		{"int8",
+			args{[]int8{1}},
+			want{Factory{Values: &int64Values{int64Value{1, false}}, DataType: options.Int64}, false}},
+		{"int16",
+			args{[]int16{1}},
+			want{Factory{Values: &int64Values{int64Value{1, false}}, DataType: options.Int64}, false}},
+		{"int32",
+			args{[]int32{1}},
+			want{Factory{Values: &int64Values{int64Value{1, false}}, DataType: options.Int64}, false}},
+		{"int64",
+			args{[]int64{1}},
+			want{Factory{Values: &int64Values{int64Value{1, false}}, DataType: options.Int64}, false}},
+		{"uint",
+			args{[]uint{1}},
+			want{Factory{Values: &int64Values{int64Value{1, false}}, DataType: options.Int64}, false}},
+		{"uint8",
+			args{[]uint8{1}},
+			want{Factory{Values: &int64Values{int64Value{1, false}}, DataType: options.Int64}, false}},
+		{"uint16",
+			args{[]uint16{1}},
+			want{Factory{Values: &int64Values{int64Value{1, false}}, DataType: options.Int64}, false}},
+		{"uint32",
+			args{[]uint32{1}},
+			want{Factory{Values: &int64Values{int64Value{1, false}}, DataType: options.Int64}, false}},
+		{"uint64",
+			args{[]uint64{1}},
+			want{Factory{Values: &int64Values{int64Value{1, false}}, DataType: options.Int64}, false}},
+		{"string",
+			args{[]string{"foo"}},
+			want{Factory{Values: &stringValues{stringValue{"foo", false}}, DataType: options.String}, false}},
+		{"string null",
+			args{[]string{""}},
+			want{Factory{Values: &stringValues{stringValue{"NaN", true}}, DataType: options.String}, false}},
+		{"bool",
+			args{[]bool{true}},
+			want{Factory{Values: &boolValues{boolValue{true, false}}, DataType: options.Bool}, false}},
+		{"datetime",
+			args{[]time.Time{time.Date(2019, 1, 1, 0, 0, 0, 0, time.UTC)}},
+			want{Factory{Values: &dateTimeValues{dateTimeValue{time.Date(2019, 1, 1, 0, 0, 0, 0, time.UTC), false}}, DataType: options.DateTime}, false}},
+		{"unsupported",
+			args{[]complex64{complex64(1)}},
+			want{Factory{Values: nil, DataType: options.None}, true}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := SliceFactory(tt.args.data)
+			if (err != nil) != tt.want.err {
+				t.Errorf("SliceFactory() error = %v, wantErr %v", err, tt.want.err)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want.factory) {
+				t.Errorf("SliceFactory() = %v, want %v", got, tt.want.factory)
+			}
+		})
+	}
+}
+
+func TestSliceConstructor_NullFloat(t *testing.T) {
+	vals, err := SliceFactory([]float64{math.NaN()})
+	if err != nil {
+		t.Errorf("Unable to construct values from null float: %v", err)
+	}
+	val := vals.Values.Element(0).Value.(float64)
+	if !math.IsNaN(val) {
+		t.Errorf("Returned %v, want NaN", val)
+	}
+}
+
+func TestSliceConstructor_NullFloatInterface(t *testing.T) {
+	vals, err := SliceFactory([]interface{}{math.NaN()})
+	if err != nil {
+		t.Errorf("Unable to construct values from null float: %v", err)
+	}
+	val := vals.Values.Element(0).Value.(float64)
+	if !math.IsNaN(val) {
+		t.Errorf("Returned %v, want NaN", val)
+	}
+}
+
+func TestSliceConstructor_Unsupported(t *testing.T) {
+	data := []complex64{1, 2, 3}
+	_, err := SliceFactory(data)
+	if err == nil {
+		t.Errorf("Returned nil error, expected error due to unsupported type %T", data)
+	}
+}
